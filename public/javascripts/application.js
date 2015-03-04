ba056698fc487b9252eacf79896b7b211870c63e
bruce wayne <no1_ldc@hotmail.com>
2015-03-03 23:02:28 -0500

    first_ajax is done

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,15 @@
 // Javascript Goes Here~
 //
+$(document).ready(funtion(){
+  $("#login_button").on("click",function(event){
+    event.preventDefault();
+    $.ajax({
+      type:'get',
+      url:'/login'
+    }).success(function(resonse){
+      $("#login_button").hide();
+    });
+  });
+});
+
+$
